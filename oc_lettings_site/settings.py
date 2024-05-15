2a436001d212996801de48dffc4e2bb4d788ab12
DayriseA <adrien.maisak@gmail.com>
2024-05-15 02:46:18 +0200

    refactor: migrate templates, views and urls to lettings app
    - move lettings related templates to lettings app
    - move lettings related views to lettings app
    - move lettings related urls to lettings app
    - create a url namespace for lettings app and update related urls

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "oc_lettings_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [BASE_DIR.joinpath("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
